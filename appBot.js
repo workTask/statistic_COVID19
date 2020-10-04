08f4b649835582b5434c51e732e02bceb86c9565
frantsuhvika@gmail.com <frantsuhvika@gmail.com>
2020-10-04 19:35:30 +0300

    added keyboard country

--- appBot.js
+++ appBot.js
@@ -2,9 +2,23 @@ require('dotenv').config();
 
 const { Telegraf } = require('telegraf');
 const api = require('covid19-api');
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+
  
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply(`Привіт ${ctx.message.from.first_name}! Цей  ЧАТ надає оновлені дані про випадки коронавірусу в режимі реального часу зі сторінки світових метрів та інших важливих веб-сайтів, що надаються найвідомішими організаціями та статистичними управліннями у світі.`))
+bot.start((ctx) => ctx.replyWithHTML(
+    `Привіт ${ctx.message.from.first_name}!
+     Цей  ЧАТ надає оновлені дані про випадки коронавірусу в режимі реального часу зі сторінки світових метрів та інших важливих веб-сайтів, що надаються найвідомішими організаціями та статистичними управліннями у світі.
+     `,
+     Markup.keyboard([
+             ['<i>Ukraine</i>','Hungary'], 
+             ['Slovakia', 'Moldova'],
+             ['Poland','Belarus']
+         ])
+         .resize()
+         .extra()
+     ));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 
 bot.on('text', async (ctx) => {
